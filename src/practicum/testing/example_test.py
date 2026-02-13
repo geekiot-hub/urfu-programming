d68ae689b3b5019281c0469126a45c99b1292b61
Kirill Samoylenkov <geekiot@proton.me>
2026-02-14 01:08:40 +0500

    Update example test

--- src/practicum/testing/example_test.py
+++ src/practicum/testing/example_test.py
@@ -1,10 +1,22 @@
+import pytest
+
+
 def one_more(x):
     return x + 1
 
 
-def test_correct():
+def decision(divident, divisor):
+    return divident / divisor
+
+
+def test_one_more_correct():
     assert one_more(4) == 5
 
 
-def test_fail():
+def test_one_more_fail():
     assert one_more(x=5) == 9
+
+
+def test_decision_error():
+    with pytest.raises(ZeroDivisionError):
+        decision(1, 0)
